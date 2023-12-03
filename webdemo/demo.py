248ef132f0399c13f119481161671930e6e6afa4
khammerm <khammermeister44@gmail.com>
2023-12-03 16:29:16 -0500

    final working web demo

--- webdemo/demo.py
+++ webdemo/demo.py
@@ -18,6 +18,13 @@ def hello_world():
 @app.route('/', methods=['POST'])
 def predict():
     
+    label_map = {
+    'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7,
+    'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14,
+    'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21,
+    'W': 22, 'X': 23, 'Y': 24, 'Z': 25, 'del': 26, 'nothing': 27, 'space': 28
+    }
+    key_list = list(label_map.keys())
     
     imagefile = request.files['imagefile']
     image_path = "./images/" + imagefile.filename
@@ -28,15 +35,18 @@ def predict():
     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
     image = preprocess_input(image)
     yhat = model.predict(image)
-    # prediction = model.prediction(image)
+    print(yhat)
+    print(np.argmax(yhat))
+    label=[]
+    label.append(key_list[np.argmax(yhat)])
     
-    prediction = np.argmax(yhat[0])
+    # for i in range(len(key_list)):
+    #     if(label[i]) == label[0]:
+    #         label.append(key_list[i])
+    #         break
     
-    label = decode_predictions(prediction)
-    label = label[0][0]
-
-    classification = '%s (%.2f%%)' % (label[1], label[2]*100)
-
+    classification = label[0]
+    # classification = '%s (%.2f%%)' % (label[1], label[0])
 
     return render_template('index.html', prediction=classification)
     
